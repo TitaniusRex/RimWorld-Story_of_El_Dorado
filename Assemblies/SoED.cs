2a8bf8962e15fe11e62f8c4753ac9c111bc92e67
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-02-07 07:02:20 +0300

    Update SoED.cs

--- Assemblies/SoED.cs
+++ Assemblies/SoED.cs
@@ -1,1 +1,29 @@
 // SoED
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace SoED {
+
+	[DefOf]
+	public static class ThingDefOf
+{
+	public static ThingDef Apparel_SoEDGoldSkin;
+}
+	[DefOf]
+	public class ThoughtWorker_GoldingSkinMerikaFackYeah : ThoughtWorker
+{
+	protected override ThoughtState CurrentStateInternal(Pawn p)
+	{
+			List<Apparel> wornApparel = p.apparel.WornApparel;
+			for (int i = 0; i < wornApparel.Count; i++)
+			{
+				if (wornApparel[i].def == ThingDefOf.Apparel_SoEDGoldSkin)
+				{
+					return true;
+				}
+			}
+		return ThoughtState.Inactive;
+	}
+}
+}
